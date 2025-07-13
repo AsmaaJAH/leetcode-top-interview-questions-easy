5aef7b9709cff2ab61b9850204789b6bc5f61434
AsmaaJAH <asmaagamal.nagy@gmail.com>
2025-07-13 20:05:45 +0300

    best day to buy or sell

---
+++ Array/BestDayToBuyOrSell/best_day_to_pay_or_sell.dart
@@ -0,0 +1,79 @@
+/*
+
+Best Time to Buy and Sell Stock II
+
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 104
+0 <= prices[i] <= 104
+
+ */
+
+class Solution {
+  // bool isAscending(List<int> nums) =>
+  //     nums.asMap().entries.every((e) => e.key == 0 || nums[e.key] >= nums[e.key - 1]);
+
+  // bool isDescending(List<int> nums) =>
+  //     nums.asMap().entries.every((e) => e.key == 0 || nums[e.key] <= nums[e.key - 1]);
+
+  int maxProfit(List<int> prices) {
+    // if (isAscending(prices)) {
+    //   return prices[prices.length - 1] - prices[0];
+    // }
+    // if (isDescending(prices)) {
+    //   return 0;
+    // }
+    int profit = 0;
+    for (int i = 1; i < prices.length ; i++) {
+      if (prices[i] > prices[i - 1]) {
+        profit += prices[i] - prices[i - 1];
+      
+      }
+    }
+    return profit;
+  }
+}
+/*
+Greedy Algorithm.
+
+The trick is:
+If prices[i] > prices[i-1], then we can add (prices[i] - prices[i-1]) to our profit.
+Because even if we sold and bought back every day, the net result would be the same.
+
+
+Why does this work?
+Instead of finding the exact days to buy & sell, we simply:
+
+Collect all the positive price differences between consecutive days.
+
+These positive differences together represent the maximum possible profit, because each increase is an independent opportunity to make money.
+
+ */
