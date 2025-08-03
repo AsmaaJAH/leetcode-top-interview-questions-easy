502ce5122d34353ebc096aa264d08373f836b40b
AsmaaJAH <asmaagamal.nagy@gmail.com>
2025-08-03 20:04:33 +0300

    list node

--- LinkedLists/ReverseLinkedList/ReverseLinkedList.java
+++ LinkedLists/ReverseLinkedList/ReverseLinkedList.java
@@ -32,17 +32,18 @@ Follow up: A linked list can be reversed either iteratively or recursively. Coul
 */
 
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+
+ 
 class Solution {
+     // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode reverseList(ListNode head) {
         ListNode previous= null;
         ListNode current =head; 
