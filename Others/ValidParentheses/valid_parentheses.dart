5ce596f7a3d339e1372fe9ccd0b053392f3bc738
AsmaaJAH <asmaagamal.nagy@gmail.com>
2025-08-17 19:40:29 +0300

    Valid Parentheses

---
+++ Others/ValidParentheses/valid_parentheses.dart
@@ -0,0 +1,87 @@
+/*
+
+Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+
+Output: false
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+
+Hint #1  
+Use a stack of characters.
+
+
+Hint #2  
+When you encounter an opening bracket, push it to the top of the stack.
+
+
+Hint #3  
+When you encounter a closing bracket, check if the top of the stack was the opening for it. If yes, pop it from the stack. Otherwise, return false.
+
+*/
+class Solution {
+  bool isValid(String s) {
+        List<String> stack = [];
+        Map<String,String> pairs = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        };
+        for (var char in s.split("")){
+            if("{[(".contains(char)){
+                stack.add(char);                
+            }else{
+                if(stack.isEmpty){
+                    return false;
+                }
+                String top= stack.removeLast();
+                if("}])".contains(char) && top != pairs[char]){
+                    return false;
+                }
+            }
+        }
+      return stack.isEmpty;
+  }
+}
